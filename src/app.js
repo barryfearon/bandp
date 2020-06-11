766ebd9a27d7de866fd3ed65fb0e9c6a675b10c8
Barry Fearon <bfearon@iowaycorp.com>
2020-06-11 21:00:56 +0100

    built 3 sections for front page

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import 'bootstrap';
 
+import '@fortawesome/fontawesome-svg-core';
+
+import '@fortawesome/free-regular-svg-icons';
+
+import '@fortawesome/free-solid-svg-icons';
+
 import './scss/app.scss';
 
 import 'lazysizes';
